2987216fe8b9e1d5ac6e26fb68855b73a69e3826
AdamARGS <aashafer@args.us>
2026-03-16 20:10:24 -0400

    various tweaks

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -91,8 +91,8 @@ public class Vision {
 
     // If multi-tag solve, trust it more
     if (tagCount > 1) {
-      linearStdDev *= VisionConstants.linearStdDevMegatag2Factor;
-      angularStdDev *= VisionConstants.angularStdDevMegatag2Factor;
+      linearStdDev *= VisionConstants.linearStdDevMultitagFactor;
+      angularStdDev *= VisionConstants.angularStdDevMultitagFactor;
     }
 
     // Per-camera trust factor
@@ -109,6 +109,7 @@ public class Vision {
   }
 
   public void toggleDriverCam() {
-    camera2.setDriverMode(!isDriverMode);
+    isDriverMode = !isDriverMode;
+    camera2.setDriverMode(isDriverMode);
   }
 }
